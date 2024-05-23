1340eaf3f732436921cf896786bc7b20171e879a
Gabriel Lima Ferreira <gabriellf0972@hotmail.com>
2024-05-23 18:58:21 -0300

    Aplicando teste das classes

--- Program.cs
+++ Program.cs
@@ -1,3 +1,12 @@
 ﻿using DesafioPOO.Models;
 
 // TODO: Realizar os testes com as classes Nokia e Iphone
+Console.WriteLine("Executando teste no Nokia");
+Smartphone nokia = new Nokia(numero: "11976778738", modelo: "tijolão", imei: "123456789", memoria: 32);
+nokia.Ligar();
+nokia.InstalarAplicativo("Nubank");
+
+Console.WriteLine("Executando teste no Iphone");
+Smartphone iphone = new Iphone(numero: "9120398388", modelo: "5c", imei: "873737282", memoria: 64);
+iphone.Ligar();
+iphone.InstalarAplicativo("NetFlix");
